ee2269a4a6bfbc1143177f11f1ec6039d2bfe69c
user <user@users-MacBook-Pro.local>
2022-03-15 16:42:59 +0800

    add dist

--- docs/webpack.dev.server.js
+++ docs/webpack.dev.server.js
@@ -13,7 +13,7 @@ const serverOptions = {
   },
 };
 
-const PORT = process.env.RCS_PORT || 3000;
+const PORT = process.env.RCS_PORT || 4000;
 
 new WebpackDevServer(webpack(webpackConfig), serverOptions).listen(
   PORT,
